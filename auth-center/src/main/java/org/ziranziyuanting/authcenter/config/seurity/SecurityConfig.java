5592957cf6b79909d6ed33a018ce63b0e77413db
yanglu <yanglucisco@163.com>
2025-11-27 20:30:58 +0800

    feat: 资质分类服务，使用授权服务的登录功能

--- auth-center/src/main/java/org/ziranziyuanting/authcenter/config/seurity/SecurityConfig.java
+++ auth-center/src/main/java/org/ziranziyuanting/authcenter/config/seurity/SecurityConfig.java
@@ -1,11 +1,25 @@
 package org.ziranziyuanting.authcenter.config.seurity;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.config.Customizer;
@@ -19,24 +33,24 @@ public class SecurityConfig {
         this.customUserDetailsService = customUserDetailsService;
     }
 
-//     @Bean
-//     @Order(1)
-//     SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
-//         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-//         http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
-//                 .oidc(Customizer.withDefaults()) // Enable OpenID Connect 1.0
-//         ;
-//         http
-//                 // 配置异常处理，当需要认证时重定向到我们的自定义登录页
-//                 .exceptionHandling(exceptions -> exceptions
-//                         .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/custom-login")))
-//                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()
-//                 // jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()) //
-//                 // 应用自定义转换器
-//                 ));
+    @Bean
+    @Order(1)
+    SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .oidc(Customizer.withDefaults()) // Enable OpenID Connect 1.0
+        ;
+        http
+                // 配置异常处理，当需要认证时重定向到我们的自定义登录页
+                .exceptionHandling(exceptions -> exceptions
+                        .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/custom-login")))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()
+                // jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()) //
+                // 应用自定义转换器
+                ));
 
-//         return http.formLogin(Customizer.withDefaults()).build();
-//     }
+        return http.formLogin(Customizer.withDefaults()).build();
+    }
 
     @Bean
     @Order(2)
@@ -60,4 +74,73 @@ public class SecurityConfig {
                 );
         return http.build();
     }
+
+    @Bean
+    JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        // 设置 JWT 令牌中权限声明（claims）的名称，默认为 "scope" 或 "scopes"
+        // 如果你的角色信息放在另一个claim里，修改这个值，例如 "roles"
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
+        // 设置权限前缀，如果不需要前缀可以设置为空字符串，这里设置为空以便直接使用自定义角色名
+        grantedAuthoritiesConverter.setAuthorityPrefix("");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
+
+
+    @Bean
+    RegisteredClientRepository registeredClientRepository() {
+        RegisteredClient certificationCatalogClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("certification-catalog")
+                .clientSecret("{noop}secret")
+                .clientName("Certification Catalog")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantTypes(gts -> {
+                    gts.add(AuthorizationGrantType.AUTHORIZATION_CODE);
+                    gts.add(AuthorizationGrantType.REFRESH_TOKEN);
+                    // gts.add(AuthorizationGrantType.CLIENT_CREDENTIALS);
+                })
+                .redirectUris((uris -> {
+                    uris.add("http://127.0.0.1:20000/login/oauth2/code/certification-catalog-oidc");
+                    
+                }))
+                .scopes(s -> {
+                    s.add("openid");
+                //     s.add("articles.read");
+                    // s.add("server");
+                })
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .tokenSettings(TokenSettings.builder().refreshTokenTimeToLive(Duration.ofMinutes(3)).build())
+                .build();
+
+        RegisteredClient articlesClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("articles-client")
+                .clientSecret("{noop}secret")
+                .clientName("Articles Client")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantTypes(gts -> {
+                    gts.add(AuthorizationGrantType.AUTHORIZATION_CODE);
+                    gts.add(AuthorizationGrantType.REFRESH_TOKEN);
+                    // gts.add(AuthorizationGrantType.CLIENT_CREDENTIALS);
+                })
+                .redirectUris((uris -> {
+                    uris.add("http://127.0.0.1:20000/login/oauth2/code/articles-client-oidc");
+                    
+                }))
+                .scopes(s -> {
+                    s.add("openid");
+                    s.add("articles.read");
+                    // s.add("server");
+                })
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .tokenSettings(TokenSettings.builder().refreshTokenTimeToLive(Duration.ofMinutes(3)).build())
+                .build();
+        List<RegisteredClient> clients = new ArrayList<>();
+        clients.add(articlesClient);
+
+        return new InMemoryRegisteredClientRepository(clients);
+    }
+
 }
---
+++ certification-catalog/src/main/java/org/ziranziyuanting/certificationcatalog/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package org.ziranziyuanting.certificationcatalog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        // .requestMatchers("/test/**").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2Login(
+                        oauth2Login -> oauth2Login.loginPage(
+                                "/oauth2/authorization/articles-client-oidc"))
+                .oauth2Client(withDefaults());
+        return http.build();
+    }
+}
