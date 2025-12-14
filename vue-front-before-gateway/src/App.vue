e086cec71888022e5351621ae5a459d58654932a
yanglu <yanglucisco@163.com>
2025-12-14 15:18:46 +0800

    feat: 成功集成了taiwindcss

--- vue-front-before-gateway/src/App.vue
+++ vue-front-before-gateway/src/App.vue
@@ -1,4 +1,4 @@
-<!-- <script setup>
+<script setup>
 import i18n from '@/locales'
 
 const locale = i18n.global.messages.value[i18n.global.locale.value].lang
@@ -14,20 +14,12 @@ const locale = i18n.global.messages.value[i18n.global.locale.value].lang
 </template>
 
 <style scoped>
-</style> -->
+</style>
 <!-- src/App.vue -->
-<template>
+<!-- <template>
   <div>
-    <p class="text-gray-400">
+    <p class="bg-lightBlue">
           成功集成 Tailwind CSS！
     </p>
   </div>
-</template>
-
-<script setup>
-// Vue 3 Composition API
-</script>
-
-<style>
-/* 可以保留原有的样式或清空 */
-</style>
+</template> -->
--- vue-front-before-gateway/src/layout/index.vue
+++ vue-front-before-gateway/src/layout/index.vue
@@ -6,16 +6,54 @@
     <div>
         <button @click="test">退出登录</button>
     </div> -->
-    <a-layout>
-        <h1>1</h1>
-    </a-layout>
-    <a-layout>
-        <h1>2</h1>
-    </a-layout>
+    <div>
+        <!-- <div>
+            <p class="bg-lightBlue">
+                成功集成 Tailwind CSS！
+            </p>
+        </div>
+        <div>
+            <button @click="btnClick">展开/收起</button>
+            <h1>
+                {{ menuIsCollapse }}
+            </h1>
+        </div> -->
+        <a-layout>
+            <a-layout-sider :collapsed="menuIsCollapseA" collapsible :theme="sideTheme" width="210">
+                <header>
+                    <h1>header</h1>
+                </header>
+            </a-layout-sider>
+            <a-layout>
+                <h1>h2</h1>
+            </a-layout>
+        </a-layout>
+        <!-- <a-layout>
+            <h1>2</h1>
+        </a-layout> -->
+    </div>
 </template>
 <script setup>
 import { cleanToken, getUserIdToken } from '../userInfo'
 import { RouterLink } from 'vue-router'
+import { ThemeModeEnum } from '@/utils/enum'
+import { computed, ref } from 'vue'
+const menuIsCollapseA = ref(false)
+// const theme = computed(() => {
+//     return store.theme
+// })
+const sideTheme = computed(() => {
+    return ThemeModeEnum.REAL_DARK
+    // return ThemeModeEnum.REAL_DARK // ThemeModeEnum.DARK 
+    // return theme.value === ThemeModeEnum.REAL_DARK ? ThemeModeEnum.DARK : theme.value
+})
+const menuIsCollapse = computed(() => {
+    return menuIsCollapseA
+})
+const btnClick = () => {
+    menuIsCollapseA.value = !menuIsCollapseA.value
+    // console.log('menuIsCollapseA:' + menuIsCollapse.value)
+}
 const test = () => {
     console.log('cleanToken')
     const idToken = getUserIdToken()
